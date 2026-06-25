a5853e3e938a84f7fb0c7829a00497f363c06f38
Ivan2007543 <simi990504@gmail.com>
2026-06-24 21:52:13 -0600

    poner Pages

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  base: '/minispa/', 
 })
